a20a6a91d0b5a4ac3407e222dc404c04b79066cd
abuza <ak3776@drexel.edu>
2024-07-10 04:28:59 -0400

    Almost finished, need to add school section

--- src/Components/textScreen.jsx
+++ src/Components/textScreen.jsx
@@ -4,26 +4,33 @@ import './textScreen.css'
 function textScreen({ content }) {
 
     const linkLines = content.links.map((link, index) => (
-        `const link${index + 1} = <a href="${link}" target="_blank">"${link}"</a>;`
+        `<p style="color: #86D8F9;">const</p> <p style="color: #C28FE4; margin-left: 10px;">link${index + 1} </p> = <a href="${link}" target="_blank">"${link}";</a>`
       ));
+
+    const bulletLines = content.bullets.map((bullet, index) => (
+        `<p style="color: #C3E88D;"> &nbsp "${bullet.bullet} ,"</p>`
+    ));
 
     const lines = [
         `&nbsp;`,
         `&nbsp;`,
-        `var name = "Abu Zaid Khan";`,
-        `var jobName = "${content.jobName}";`,
+        `<p style="color: #86D8F9;">const </p> <p style="color: #C28FE4;"> Name </p> = <p style="color: #C3E88D;">"${content.jobName}"</p>`,
+        `&nbsp;`,
+        `<p style="color: #86D8F9;">let</p> <p style="color: #C28FE4;"> startDate </p> = <p style="color: #C3E88D;">"${content.startDate}"</p>`,
+        `<p style="color: #86D8F9;">let</p> <p style="color: #C28FE4;"> endDate </p> = <p style="color: #C3E88D;">"${content.endDate}"</p>`,
+        `&nbsp;`,
+        `<p style="color: #86D8F9;">const</p> <p style="color: #C28FE4; margin-left: 10px;">URL </p> = <a href="${content.URL}" target="_blank">"${content.URL}"</a>;`,
         `&nbsp;`,
         `&nbsp;`,
-        `const jobDesc = "${content.description}";`,
+        `<p style="color: #86D8F9;">let</p> <p style="color: #C28FE4; margin-left: 10px;">jobDesc</p> <p>=</p> <p style="color: #C3E88D;"> "${content.description}" ;</p>`,
         `&nbsp;`,
+        `<p style="color: #86D8F9;">const</p> <p style="color: #C28FE4; margin-left: 10px;">bulletPoints</p> = <p style="color: #ffff00;">{</p>`,
+        ...bulletLines,
+        `<p style="color: #ffff00;">\};</p>`,
         `&nbsp;`,
-        `let startDate = "${content.startDate}";`,
-        `let endDate = "${content.endDate}";`,
         `&nbsp;`,
         ...linkLines,
         `&nbsp;`,
-        `const jobURL = <a href="${content.URL}" target="_blank">"${content.URL}"</a>;`,
-        `&nbsp;`
       ];
 
     return (
